07b853b459a92db430ba0a2d15d3629b6c060dd2
Jeremy <jeremykung@gmail.com>
2022-02-11 09:21:30 +0800

    added time test

---
+++ addUpTo.js
@@ -0,0 +1,29 @@
+// First algorithm
+const addUpTo1 = n => {
+    let total = 0
+    for (let i=1; i<=n; i++) {
+        total += i
+    }
+    console.log(total)
+    return total
+}
+
+// Second algorithm
+const addUpTo2 = n => {
+    return n * (n + 1) / 2
+}
+
+// Set start time
+let t1 = performance.now()
+// Add algorithm below...
+addUpTo1(1000000000)
+// Set end time
+let t2 = performance.now()
+// Show time elapsed
+console.log(`Time Elapsed: ${ (t2 - t1) / 1000 }`)
+
+/*
+Problem with Time
+- machines perform at different speeds
+- for fast algorithms, speed might not be precise enough (they are all too fast)
+*/
